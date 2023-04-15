8930f8e7bcb1cd5a11d7f3276b0d3ecfb0987cb7
Andrew (kaya6) <kaya6@mcmaster.ca>
2023-04-15 17:18:09 -0400

    Added some text for each section.

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -5,7 +5,20 @@ const Contact = () => {
         <div className="contactSection" id="contactSectionID">
             <h1>Contact</h1>
             <div>
-            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                The best way to contact me is through email.
+            </div>
+            <div>
+            Helpful things to include in an email:
+            </div>
+            <ul>
+                <li>How often do you want your lawn mowed?</li>
+                <li>How big is your lawn? (A rough estimate is great).</li>
+                <li>Do you have any pets?</li>
+                <li style={{fontWeight: "bold"}}>Your address</li>          
+            </ul>
+
+            <div style={{fontWeight: "bold"}}>
+                Your address is the most important thing to include - it allows me to quote you in an accurate and timely manner. 
             </div>
         </div>
     );
